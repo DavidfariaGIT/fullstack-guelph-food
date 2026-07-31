2fb58aae4a2188c42637a27469531d910c43c7c6
DavidfariaGIT <davidfaria17@gmail.com>
2026-07-31 16:45:44 -0400

    created auth path for register users, added checks for fields and testing formats

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import { apiRouter } from './routes/getRouter.js'
+import { authRouter } from './routes/auth.js'
 
 const PORT = 8000
 const app = express()
 
 app.use(express.static('public')) 
+app.use(express.json())
 
 app.use('/api', apiRouter)
 app.use('/api/restaurants', apiRouter)
+app.use('/api/auth', authRouter)
 
 
 app.listen(PORT, () => { 
